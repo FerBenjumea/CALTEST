7d0cfddeac724b2863c46263f56f9f26e2106e1f
FernandoBA <fernando.benjumea.argudo@gmail.com>
2024-09-30 11:34:30 +0200

    Añadido metodo mostrarSaludo()

--- EPD01/P1/Main.java
+++ EPD01/P1/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         mostrarSaludo();
-        mostrarMensajeGitHub();
     }
 
-    public static void mostrarSaludo() {
+   public static void mostrarSaludo() {
         System.out.println("Hola clase de EPD 1");
-    }
-
-    public static void mostrarMensajeGitHub() {
-        System.out.println("Esto ha sido hecho en una clase de GitHub");
     }
 }
